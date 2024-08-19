a825b93c65421d2444374d537afa3beea4f3f2f8
IDbok <bokarev.fic@gmail.com>
2024-08-19 10:42:52 +0400

    Дополненны моменты устранённых замечаний из слияния c HotFix + настроено правило ViewMode для выпущенных ТК

--- TC_WinForms/WinForms/Work/Coefficient.Designer.cs
+++ TC_WinForms/WinForms/Work/Coefficient.Designer.cs
@@ -62,9 +62,11 @@
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(textBox1);
-            groupBox1.Location = new Point(12, 51);
+            groupBox1.Location = new Point(15, 64);
+            groupBox1.Margin = new Padding(4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(490, 99);
+            groupBox1.Padding = new Padding(4);
+            groupBox1.Size = new Size(612, 124);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Время действия, мин.";
@@ -72,53 +74,59 @@
             // panel1
             // 
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(6, 50);
+            panel1.Location = new Point(8, 62);
+            panel1.Margin = new Padding(4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(105, 24);
+            panel1.Size = new Size(131, 30);
             panel1.TabIndex = 5;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Dock = DockStyle.Right;
-            label1.Location = new Point(88, 0);
+            label1.Location = new Point(109, 0);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(17, 20);
+            label1.Size = new Size(22, 25);
             label1.TabIndex = 0;
             label1.Text = "5";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(363, 50);
+            label4.Location = new Point(454, 62);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(17, 20);
+            label4.Size = new Size(22, 25);
             label4.TabIndex = 4;
             label4.Text = "5";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(348, 50);
+            label3.Location = new Point(435, 62);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(19, 20);
+            label3.Size = new Size(24, 25);
             label3.TabIndex = 3;
             label3.Text = "=";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(177, 24);
+            label2.Location = new Point(221, 30);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(71, 20);
+            label2.Size = new Size(86, 25);
             label2.TabIndex = 2;
             label2.Text = "формула";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(117, 47);
+            textBox1.Location = new Point(146, 59);
+            textBox1.Margin = new Padding(4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(225, 27);
+            textBox1.Size = new Size(280, 31);
             textBox1.TabIndex = 1;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
@@ -129,9 +137,11 @@
             groupBox2.Controls.Add(label7);
             groupBox2.Controls.Add(label8);
             groupBox2.Controls.Add(textBox2);
-            groupBox2.Location = new Point(12, 156);
+            groupBox2.Location = new Point(15, 195);
+            groupBox2.Margin = new Padding(4);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(490, 99);
+            groupBox2.Padding = new Padding(4);
+            groupBox2.Size = new Size(612, 124);
             groupBox2.TabIndex = 6;
             groupBox2.TabStop = false;
             groupBox2.Text = "Пример";
@@ -139,115 +149,128 @@
             // panel2
             // 
             panel2.Controls.Add(label5);
-            panel2.Location = new Point(6, 50);
+            panel2.Location = new Point(8, 62);
+            panel2.Margin = new Padding(4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(105, 24);
+            panel2.Size = new Size(131, 30);
             panel2.TabIndex = 5;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Dock = DockStyle.Right;
-            label5.Location = new Point(88, 0);
+            label5.Location = new Point(109, 0);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(17, 20);
+            label5.Size = new Size(22, 25);
             label5.TabIndex = 0;
             label5.Text = "5";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(363, 50);
+            label6.Location = new Point(454, 62);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(25, 20);
+            label6.Size = new Size(32, 25);
             label6.TabIndex = 4;
             label6.Text = "25";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(348, 50);
+            label7.Location = new Point(435, 62);
+            label7.Margin = new Padding(4, 0, 4, 0);
             label7.Name = "label7";
-            label7.Size = new Size(19, 20);
+            label7.Size = new Size(24, 25);
             label7.TabIndex = 3;
             label7.Text = "=";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(177, 24);
+            label8.Location = new Point(221, 30);
+            label8.Margin = new Padding(4, 0, 4, 0);
             label8.Name = "label8";
-            label8.Size = new Size(71, 20);
+            label8.Size = new Size(86, 25);
             label8.TabIndex = 2;
             label8.Text = "формула";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(117, 47);
+            textBox2.Location = new Point(146, 59);
+            textBox2.Margin = new Padding(4);
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
-            textBox2.Size = new Size(225, 27);
+            textBox2.Size = new Size(280, 31);
             textBox2.TabIndex = 1;
             textBox2.Text = "*5";
             // 
             // groupBox3
             // 
             groupBox3.Controls.Add(flowLayoutPanel1);
-            groupBox3.Location = new Point(12, 261);
+            groupBox3.Location = new Point(15, 326);
+            groupBox3.Margin = new Padding(4);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(490, 235);
+            groupBox3.Padding = new Padding(4);
+            groupBox3.Size = new Size(612, 294);
             groupBox3.TabIndex = 7;
             groupBox3.TabStop = false;
-            groupBox3.Text = "Комментарии времени";
+            groupBox3.Text = "Правила расчета времени тех. перехода";
             // 
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.AutoScroll = true;
             flowLayoutPanel1.Controls.Add(label9);
-            flowLayoutPanel1.Location = new Point(6, 26);
+            flowLayoutPanel1.Location = new Point(8, 32);
+            flowLayoutPanel1.Margin = new Padding(4);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(484, 203);
+            flowLayoutPanel1.Size = new Size(605, 254);
             flowLayoutPanel1.TabIndex = 0;
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(3, 0);
+            label9.Location = new Point(4, 0);
+            label9.Margin = new Padding(4, 0, 4, 0);
             label9.Name = "label9";
-            label9.Size = new Size(50, 20);
+            label9.Size = new Size(59, 25);
             label9.TabIndex = 0;
             label9.Text = "label9";
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(18, 19);
+            label10.Location = new Point(22, 24);
+            label10.Margin = new Padding(4, 0, 4, 0);
             label10.Name = "label10";
-            label10.Size = new Size(58, 20);
+            label10.Size = new Size(69, 25);
             label10.TabIndex = 8;
             label10.Text = "label10";
             // 
             // button1
             // 
-            button1.Location = new Point(169, 524);
+            button1.Location = new Point(211, 655);
+            button1.Margin = new Padding(4);
             button1.Name = "button1";
-            button1.Size = new Size(185, 29);
+            button1.Size = new Size(231, 36);
             button1.TabIndex = 9;
             button1.Text = "Добавить";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // Coefficient
+            // CoefficientForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(529, 587);
+            ClientSize = new Size(661, 734);
             Controls.Add(button1);
             Controls.Add(label10);
             Controls.Add(groupBox3);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
-            Name = "Coefficient";
+            Margin = new Padding(4);
+            Name = "CoefficientForm";
             Text = "Коэффициент";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
