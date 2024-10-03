e5a4f0162824b53d925ad78bc5b5702030c1bbd9
IDbok <bokarev.fic@gmail.com>
2024-10-03 12:36:00 +0400

    Revert "release 1.5.9"
    This reverts commit 15484059b08e4c7421c878fedd946924e59608c8.

--- TC_WinForms/DataProcessing/ApplicationInfoService.cs
+++ TC_WinForms/DataProcessing/ApplicationInfoService.cs
@@ -42,7 +42,7 @@ public static class ApplicationInfoService
     }
     public static string GetApplicationVersion()
     {
-        return "1.5.9";
+        return "1.5.8";
     }
 
     public static string GetApplicationName()
