53078726b0b4c93154994eaac8492805ea49ac0f
IDbok <bokarev.fic@gmail.com>
2024-11-12 18:10:36 +0400

    Integrate Serilog for enhanced logging
    Added Serilog library and configured it in `Program.cs` using `appsettings.json`. Introduced logging in `AuthorizationService.cs` and `Win7_new.cs`. Commented out old custom logging. Updated project files to include Serilog references and ensure `appsettings.json` is copied to the output directory.

---
+++ TC_WinForms/Services/Logging/Logger.cs
@@ -0,0 +1,31 @@
+﻿//using Serilog;
+
+//namespace TC_WinForms.Services.Logging;
+
+//public class Logger : ILogger
+//{
+//    private readonly Serilog.ILogger _logger;
+
+//    public Logger()
+//    {
+//        _logger = new LoggerConfiguration()
+//            .WriteTo.Console()
+//            .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+//            .CreateLogger();
+//    }
+
+//    public void LogInformation(string message)
+//    {
+//        _logger.Information(message);
+//    }
+
+//    public void LogWarning(string message)
+//    {
+//        _logger.Warning(message);
+//    }
+
+//    public void LogError(string message, Exception ex)
+//    {
+//        _logger.Error(ex, message);
+//    }
+//}
