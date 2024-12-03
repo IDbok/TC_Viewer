8f9eb6768550c87a29ee9a053067d944fe9bf467
IDbok <bokarev.fic@gmail.com>
2024-12-03 11:26:31 +0400

    Логгирование сервиса блокировок ConcurrencyBlockService

--- TC_WinForms/Services/ConcurrencyBlockService.cs
+++ TC_WinForms/Services/ConcurrencyBlockService.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Serilog;
+using TC_WinForms.WinForms;
 using TcDbConnector;
 using TcModels.Models;
 using TcModels.Models.Interfaces;
@@ -8,6 +10,8 @@ namespace TC_WinForms.Services
 {
     public class ConcurrencyBlockService<T> where T : class, IIdentifiable
     {
+        private readonly ILogger _logger;
+
         private bool? IsObjectInUse; //используется ли сейчас карта в программе другим пользователем
         private Timer? UpdateDataTimer;
         private int ObjectId;
@@ -15,70 +19,121 @@ namespace TC_WinForms.Services
         private int TimerInterval;//интервал работы таймера в милисекундах
         public ConcurrencyBlockService(T obj, int timerInterval)
         {
+            _logger = Log.Logger.ForContext<ConcurrencyBlockService<T>>();
+
             ObjectType = obj.GetType().Name;
             ObjectId = obj.Id;
             TimerInterval = timerInterval;
+
+            Log.Information("Инициализация ConcurrencyBlockService для объекта {ObjectType} с ID={ObjectId}", ObjectType, ObjectId);
         }
         public bool GetObjectUsedStatus() 
         {
             if (IsObjectInUse == null)
                 CheckAreObjectBlock();
-            return (bool)IsObjectInUse; 
+
+            _logger.Information("Статус блокировки объекта {ObjectType} с ID={ObjectId}: {IsObjectInUse}", ObjectType, ObjectId, IsObjectInUse);
+            return (bool)IsObjectInUse!; 
         }
         public void BlockObject()
         {
             if (IsObjectInUse != null && (bool)IsObjectInUse)
-                return; 
-
-            using (MyDbContext dbContext = new MyDbContext())
             {
-                using (var transaction = dbContext.Database.BeginTransaction())
+                _logger.Warning("Попытка заблокировать уже заблокированный объект {ObjectType} с ID={ObjectId}", ObjectType, ObjectId);
+                return;
+            }
+
+            try
+            {
+                using (MyDbContext dbContext = new MyDbContext())
                 {
-                    var isBlocked = dbContext.BlockedConcurrencyObjects
+                    using (var transaction = dbContext.Database.BeginTransaction())
+                    {
+                        var isBlocked = dbContext.BlockedConcurrencyObjects
                                                  .Any(b => b.ObjectId == ObjectId && b.ObjectType == ObjectType);
 
-                    if (!isBlocked)
-                    {
-                        var blockedObject = new ObjectLocker
+                        if (!isBlocked)
                         {
-                            TimeStamp = DateTime.Now,
-                            ObjectId = ObjectId,
-                            ObjectType = ObjectType
-                        };
+                            var blockedObject = new ObjectLocker
+                            {
+                                TimeStamp = DateTime.Now,
+                                ObjectId = ObjectId,
+                                ObjectType = ObjectType
+                            };
 
-                        dbContext.BlockedConcurrencyObjects.Add(blockedObject);
-                        dbContext.SaveChanges();
-                        transaction.Commit();
+                            dbContext.BlockedConcurrencyObjects.Add(blockedObject);
+                            dbContext.SaveChanges();
+                            transaction.Commit();
+
+                            _logger.Information("Объект {ObjectType} с ID={ObjectId} успешно заблокирован", ObjectType, ObjectId);
+                        }
+                        else
+                        {
+                            _logger.Warning("Объект {ObjectType} с ID={ObjectId} уже заблокирован другим пользователем", ObjectType, ObjectId);
+                        }
                     }
                 }
+
+                SetTimer();
             }
-            SetTimer();
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Ошибка при блокировке объекта {ObjectType} с ID={ObjectId}", ObjectType, ObjectId);
+                throw;
+            }
         }
         public void CleanBlockData()
         {
             if (IsObjectInUse != null && (bool)IsObjectInUse)
-                return;
-
-            using (MyDbContext dbContext = new MyDbContext())
             {
-               
-                    var blockedObject = dbContext.BlockedConcurrencyObjects.Where(s => s.ObjectId == ObjectId && s.ObjectType == ObjectType)
-                                                                            .FirstOrDefault();
+                _logger.Warning("Попытка снять блокировку с объекта {ObjectType} с ID={ObjectId}, который используется другим пользователем", ObjectType, ObjectId);
+                return;
+            }
+
+            try
+            {
+                using (MyDbContext dbContext = new MyDbContext())
+                {
+                    var blockedObject = dbContext.BlockedConcurrencyObjects
+                                                 .FirstOrDefault(s => s.ObjectId == ObjectId && s.ObjectType == ObjectType);
+
                     if (blockedObject != null)
                     {
                         dbContext.BlockedConcurrencyObjects.Remove(blockedObject);
                         dbContext.SaveChanges();
+                        _logger.Information("Блокировка снята для объекта {ObjectType} с ID={ObjectId}", ObjectType, ObjectId);
                     }
+                    else
+                    {
+                        _logger.Warning("Попытка снять блокировку с объекта {ObjectType} с ID={ObjectId}, который не был заблокирован", ObjectType, ObjectId);
+                    }
+                }
+
+                UpdateDataTimer?.Dispose();
             }
-            UpdateDataTimer?.Dispose();
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Ошибка при снятии блокировки объекта {ObjectType} с ID={ObjectId}", ObjectType, ObjectId);
+                throw;
+            }
 
         }
         private void CheckAreObjectBlock()
         {
-            using (MyDbContext dbContext = new MyDbContext())
+            try
             {
-                IsObjectInUse = dbContext.BlockedConcurrencyObjects
-                                       .Any(s => s.ObjectId == ObjectId && s.ObjectType == ObjectType);
+                using (MyDbContext dbContext = new MyDbContext())
+                {
+                    IsObjectInUse = dbContext.BlockedConcurrencyObjects
+                                             .Any(s => s.ObjectId == ObjectId && s.ObjectType == ObjectType);
+
+                    _logger.Debug("Проверка блокировки завершена для объекта {ObjectType} с ID={ObjectId}. Статус: {IsObjectInUse}", ObjectType, ObjectId, IsObjectInUse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Ошибка при проверке блокировки объекта {ObjectType} с ID={ObjectId}", ObjectType, ObjectId);
+                throw;
             }
         }
         private void SetTimer()
@@ -89,25 +144,37 @@ namespace TC_WinForms.Services
             // Hook up the Elapsed event for the timer. 
             UpdateDataTimer.Elapsed += UpdateDataTimer_Elapsed;
             UpdateDataTimer.AutoReset = true;
-            UpdateDataTimer.Enabled = true;
+            UpdateDataTimer.Enabled = true; 
+            
+            _logger.Information("Таймер обновления блокировки установлен с интервалом {TimerInterval} мс для объекта {ObjectType} с ID={ObjectId}", TimerInterval, ObjectType, ObjectId);
         }
 
         private void UpdateDataTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
         {
-            using (MyDbContext dbContext = new MyDbContext())
+            try
             {
-                var blockedObject = dbContext.BlockedConcurrencyObjects
-                                             .FirstOrDefault(s => s.ObjectId == ObjectId && s.ObjectType == ObjectType);
+                using (MyDbContext dbContext = new MyDbContext())
+                {
+                    var blockedObject = dbContext.BlockedConcurrencyObjects
+                                                 .FirstOrDefault(s => s.ObjectId == ObjectId && s.ObjectType == ObjectType);
 
-                if (blockedObject != null)
-                {
-                    blockedObject.TimeStamp = e.SignalTime;
-                    dbContext.SaveChanges();
+                    if (blockedObject != null)
+                    {
+                        blockedObject.TimeStamp = e.SignalTime;
+                        dbContext.SaveChanges();
+                        _logger.Debug("Таймер обновил метку времени для объекта {ObjectType} с ID={ObjectId} на {TimeStamp}", ObjectType, ObjectId, blockedObject.TimeStamp);
+                    }
+                    else
+                    {
+                        _logger.Warning("Таймер не смог найти объект {ObjectType} с ID={ObjectId} для обновления метки времени. Остановка таймера", ObjectType, ObjectId);
+                        UpdateDataTimer?.Dispose();
+                    }
                 }
-                else
-                {
-                    UpdateDataTimer?.Dispose();
-                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Ошибка при обновлении таймера блокировки для объекта {ObjectType} с ID={ObjectId}", ObjectType, ObjectId);
+                UpdateDataTimer?.Dispose();
             }
         }
 
