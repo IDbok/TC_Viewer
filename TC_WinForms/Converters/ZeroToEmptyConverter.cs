d6dc7326abfabcc9fc790b874ce0cf5c41936561
AlyaLugovkina <alya.lugovkina.i@mail.ru>
2025-07-15 15:04:21 +0300

    П362 Исправлен формат вывода

--- TC_WinForms/Converters/ZeroToEmptyConverter.cs
+++ TC_WinForms/Converters/ZeroToEmptyConverter.cs
@@ -1,4 +1,4 @@
-﻿using System.Globalization;
+using System.Globalization;
 using System.Windows.Data;
 
 namespace TC_WinForms.Converters
@@ -7,7 +7,32 @@ namespace TC_WinForms.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (value is double doubleValue && doubleValue == 0) ? string.Empty : value?.ToString();
+            if (value is double doubleValue)
+            {
+                // Если значение 0 - возвращаем пустую строку
+                if (doubleValue == 0)
+                    return string.Empty;
+
+                var numberFormat = (NumberFormatInfo)culture.NumberFormat.Clone();
+                numberFormat.NumberDecimalSeparator = ",";
+
+                // Проверяем, является ли число целым
+                if (doubleValue == Math.Truncate(doubleValue))
+                {
+                    // Целое число - выводим без десятичных знаков
+                    return doubleValue.ToString("0", numberFormat);
+                }
+                else
+                {
+                    // Дробное число - максимум 2 знака после запятой
+                    return doubleValue.ToString("0.##", numberFormat);
+                }
+            }
+
+            // Для других типов (на всякий случай)
+            return value?.ToString();
+
+            //return (value is double doubleValue && doubleValue == 0) ? string.Empty : value?.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
