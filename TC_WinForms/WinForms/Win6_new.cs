97f9dfe336741d2558b041b48ed6baa24b090532
AlyaLugovkina <alya.lugovkina.i@mail.ru>
2025-08-28 09:11:15 +0300

    Добавлено отсутствуещее сообщение о блокировке карты при её открытии

--- TC_WinForms/WinForms/Win6_new.cs
+++ TC_WinForms/WinForms/Win6_new.cs
@@ -235,6 +235,7 @@ namespace TC_WinForms.WinForms
             {
                 tcViewState.IsViewMode = true;
                 TcWasBlocked = true;
+                MessageBox.Show("Сейчас карта используется другим пользователем. Она доступна только для просмотра.");
             }
             else if (isViewMode != null && tcViewState.IsViewMode != isViewMode)
             {
